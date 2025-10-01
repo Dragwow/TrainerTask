1a382694c395cd97e1851b27677f10d9efcdeb2f
yumimi <pokotylomain@gmail.com>
2025-10-01 03:09:11 +0200

    Trainer Task

--- company-service/src/main/java/com/yumimi/controller/CompanyController.java
+++ company-service/src/main/java/com/yumimi/controller/CompanyController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/companies")
+@RequestMapping("/api/companies")
 @AllArgsConstructor
 public class CompanyController {
 
